a497138cad070d2496ae50f6ef76ff6a9461e361
watarutounai <117158406+watarutounai@users.noreply.github.com>
2022-12-05 09:51:10 +0000

    [編集中]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,23 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  #belongs_to :books
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed, dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower, dependent: :destroy
+    #フォローした時の処理
+    def follow(user_id)
+      active_relationships.create(followed_id: user_id)
+    end
+    #フォローを外すときの処理
+    def unfollow(user_id)
+      active_relationships.find_by(followed_id: user_id).destroy
+    end
+    #フォローしているか判定
+    def following?(user)
+      followings.include?(user)
+    end
+
   has_one_attached :profile_image
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
