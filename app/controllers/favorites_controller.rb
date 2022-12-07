b27e19dffb213008e645816e5f3ca45be4af6ccc
watarutounai <117158406+watarutounai@users.noreply.github.com>
2022-12-07 07:56:36 +0000

    [編集中]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_back fallback_location: root_path
+    #redirect_back fallback_location: root_path
     #redirect_to book_path(book)
   end
 
@@ -11,7 +11,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_back fallback_location: root_path
+    #redirect_back fallback_location: root_path
     #redirect_to book_path(book)
   end
 end
